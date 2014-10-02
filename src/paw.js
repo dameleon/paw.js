95a0af0661cb04ed59defc81061b098c5b6ad107
dameleon <dameleon@gmail.com>
2014-10-02 20:56:46 +0900

    wip

--- src/paw.js
+++ src/paw.js
@@ -1,6 +1,9 @@
 ;(function(global, undefined) {
 'use strict';
 
+//! Object.keys Polyfill from [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys#Polyfill)
+Object.keys||(Object.keys=function(){var e=Object.prototype.hasOwnProperty,f=!{toString:null}.propertyIsEnumerable("toString"),c="toString toLocaleString valueOf hasOwnProperty isPrototypeOf propertyIsEnumerable constructor".split(" "),g=c.length;return function(b){if("object"!==typeof b&&"function"!==typeof b||null===b)throw new TypeError("Object.keys called on non-object");var d=[],a;for(a in b)e.call(b,a)&&d.push(a);if(f)for(a=0;a<g;a++)e.call(b,c[a])&&d.push(c[a]);return d}}());
+
 var document = global.document;
 var IS_SUPPORT_TOUCH = 'ontouchstart' in document;
 var EVENTS = {
@@ -11,89 +14,62 @@ var EVENTS = {
 };
 var defaultSetting = {
         cancelTimeDuration: 200,
-        cancelPixelDistance: 5,
+        workingPixel: 5,
 };
 
-function PowTouch(rootNode, ev) {
-    this.target = ev.target;
-    this.touchesIdList = this._getTouchIdList(ev);
-    this.rootNode = rootNode;
-    rootNode.addEventListener(EVENTS.MOVE, this);
-    rootNode.addEventListener(EVENTS.END, this);
-    rootNode.addEventListener(EVENTS.CANCEL, this);
+
+function PawTouch(timeoutDuration, workingPixel, touchInfo) {
+    var that = this;
+
+    this.id = touchInfo.identifier;
+    this.workingPixel = workingPixel;
+    this.target = touchInfo.target;
+    this.startX = touchInfo.pageX;
+    this.startY = touchInfo.pageY;
+    this.timer = setTimeout(function() {
+        that._timeout();
+    }, timeoutDuration);
 }
+
 
 PowTouch.prototype = {
-    constructor: PowTouch,
-    handleEvent: function() {
-        switch (ev.type) {
-            case EVENTS.MOVE:
-                this.onMove(ev);
-                break;
-            case EVENTS.END:
-                this.onEnd(ev);
-                break;
-            case EVENTS.CANCEL:
-                this.onCancel(ev);
-                break;
-        }
-    },
-    _getTouchIdList: function(ev) {
-        var res = [];
-        var touches = ev.changedTouches;
-        var len = touches.length;
-
-        if (len === 1) {
-            res[res.length] = touches[0].id;
-        } else {
-            for (var i = 0; i < length; i++) {
-                res[res.length] = touches[i].id;
-            }
-        }
-        return res;
-    },
-    onMove: function(ev) {
-        var touchesIdList = this.touchesIdList;
-        var touches = ev.changedTouches;
-        var touch;
-
-        for (var i = 0, iz = touches.length; i < iz; i++) {
-            touch = touches[i];
-            if (touchesIdList.indexOf(touch.id) > -1) {
-
-            }
-        }
-
-    },
-    onEnd: function(ev) {
-
-    },
-    onCancel: function(ev) {
-
-    },
-    dispose: function() {
-        var rootNode = this.rootNode;
-
-        rootNode.removeEventListener(EVENTS.MOVE, this);
-        rootNode.removeEventListener(EVENTS.END, this);
-        rootNode.removeEventListener(EVENTS.CANCEL, this);
-        this.rootNode = null;
-        this.idList = null;
-        this.target = null;
-    }
+    constructor: PowTouch
+    move: _move,
+    end: _end,
+    cancel: _cancel,
+    _timeout: _timeout
 };
 
-function __getTouchIds(ev) {
+
+function _move(eventInfo) {
 
 }
+
+function _end(eventInfo) {
+
+}
+
+function _cancel(eventInfo) {
+
+}
+
+function _timeout() {
+
+}
+
 
 function Paw(rootNode, option) {
     if (rootNode && !rootNode.addEventListener) {
         throw new Error();
     }
-    this.setting = __extend({}, defaultSetting, option || {});
+    var setting = this.setting = {};
+
+    for (var key in defaultSetting) {
+        setting[key] = option[key] || defaultSetting[key];
+    }
     this.rootNode = rootNode || document;
     this.rootNode.addEventListener(EVENTS.START, this);
+    this.handlers = {};
 }
 
 Paw.prototype = {
@@ -102,7 +78,9 @@ Paw.prototype = {
     onStart: _onStart,
     onMove: _onMove,
     onEnd: _onEnd,
-    onCancel: _onCancel
+    onCancel: _onCancel,
+    bindEvents: _bindEvents,
+    unbindEvents: _unbindEvents
 };
 
 function _handleEvent(ev) {
@@ -123,74 +101,93 @@ function _handleEvent(ev) {
 }
 
 function _onStart(ev) {
+    var setting = this.setting;
+    var handlers = this.handlers;
+    var touches = __getTouchInfoList(ev);
+    var handler;
 
+    for (var i = 0, iz = touches.length; i < iz; i++) {
+        handler = new PowTouch(setting, touches[i]);
+        handlers[handler.id] = handler;
+    }
+    this.bindEvents();
 }
 
 function _onMove(ev) {
+    var handlers = this.handlers;
+    var touches = __getTouchInfoList(ev);
+    var touchInfo, handler;
 
+    for (var i = 0, iz = touches.length; i < iz; i++) {
+        touchInfo = touches[i];
+        handler = handlers[touchInfo.identifier];
+        if (handler) {
+            handler.move(touchInfo);
+        }
+    }
 }
 
 function _onEnd(ev) {
+    var handlers = this.handlers;
+    var touches = __getTouchInfoList(ev);
+    var touchInfo, handler, id;
 
+    for (var i = 0, iz = touches.length; i < iz; i++) {
+        touchInfo = touches[i];
+        id = touchInfo.identifier;
+        handler = handlers[id];
+        if (handler) {
+            handler.end(touchInfo);
+        }
+        handlers[id] = null;
+    }
+    this.unbindEvents();
 }
 
 function _onCancel(ev) {
+    var handlers = this.handlers;
+    var touches = __getTouchInfoList(ev);
+    var touchInfo, handler, id;
 
-}
-
-
-
-
-function __getEvent(ev, key) {
-
-}
-
-function __extend() {
-    if (arguments.length < 2) {
-        return arguments[0];
-    }
-    var deepTargetRe = /(object|array)/;
-    var args = [].slice.call(arguments);
-    var res = args.shift();
-    var i = 0, arg;
-
-    while ((arg = args[i])) {
-        var j = 0;
-
-        switch (typeof arg) {
-            case 'array':
-                for (var jz = arg.length; j < jz; j++) {
-                    _extend(j, res, arg);
-                }
-                break;
-            case 'object':
-                var donorKeys = Object.keys(arg);
-
-                for (var key; key = donorKeys[j]; j++) {
-                    _extend(key, res, arg);
-                }
-                break;
+    for (var i = 0, iz = touches.length; i < iz; i++) {
+        touchInfo = touches[i];
+        id = touchInfo.identifier;
+        handler = handlers[id];
+        if (handler) {
+            handler.cancel(touchInfo);
         }
-        i++;
+        handlers[id] = null;
     }
-
-    return res;
-
-    function _extend(key, target, donor) {
-        var val = donor[key];
-        var targetVal = target[key];
-        var donorValType = (val && typeof val) || '';
-        var targetValType = (targetVal && typeof targetVal) || '';
-
-        if (deepTargetRe.test(donorValType)) {
-            if (targetValType !== donorValType) {
-                target[key] = (donorValType === 'object') ? {} : [];
-            }
-            __extend(target[key], val);
-        } else {
-            target[key] = val;
-        }
-    }
+    this.unbindEvents();
 }
+
+function _bindEvents() {
+    if (this.boundEvents) {
+        return;
+    }
+    var rootNode = this.rootNode;
+
+    this.boundEvents = true;
+    rootNode.addEventListener(EVENTS.MOVE, this);
+    rootNode.addEventListener(EVENTS.END, this);
+    rootNode.addEventListener(EVENTS.CANCEL, this);
+}
+
+function _unbindEvents() {
+    if (!this.boundEvents || Object.keys(this.handlers).length > 0) {
+        return;
+    }
+    var rootNode = this.rootNode;
+
+    this.boundEvents = false;
+    rootNode.removeEventListener(EVENTS.MOVE, this);
+    rootNode.removeEventListener(EVENTS.END, this);
+    rootNode.removeEventListener(EVENTS.CANCEL, this);
+}
+
+function __getTouchInfoList(ev) {
+    return IS_SUPPORT_TOUCH ? ev.changedTouches || [ev];
+}
+
 
 })(this.self || global, void 0);
