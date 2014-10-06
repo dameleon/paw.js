aee737f49d44a0c0d0f85e2d800a67459d81a6e5
dameleon <dameleon@gmail.com>
2014-10-06 22:11:46 +0900

    add Paw.createTouchEvent method

--- src/paw.js
+++ src/paw.js
@@ -36,7 +36,6 @@ function Paw(rootNode, option) {
     this.timers = {};
 }
 
-Paw.keys = Object.keys || (_ && _.keys);
 Paw.IS_SUPPORT_TOUCH_EVENT = IS_SUPPORT_TOUCH_EVENT;
 Paw.IS_SUPPORT_POINTER_EVENT = IS_SUPPORT_POINTER_EVENT;
 Paw.EVENT_TYPES = {
@@ -50,6 +49,115 @@ Paw.EVENT_INTERFACES = {
     POINTER_EVENT : 'pointerevent',
     MOUSE_EVENT   : 'mouseevent'
 };
+Paw.keys = Object.keys || (_ && _.keys);
+Paw.env = __getEnvData(global.navigator.userAgent);
+Paw.createTouchEvent = (function(env) {
+    var _defaultView = document.defaultView;
+
+    if (env.isAndroid && (env.version && env.version[0] < 4)) {
+        return function(type, target, bubbles, cancelable, pageX, pageY, identifier) {
+            var event = document.createEvent('MouseEvent');
+            var touchList = _getTouchList(target, identifier, pageX, pageY);
+
+            event.initMouseEvent(
+                type,
+                bubbles,
+                cancelable,
+                _defaultView,
+                1, // detail
+                0, // screenX
+                0, // screenY
+                pageX,
+                pageY,
+                false, // ctrlKey
+                false, // altKey
+                false, // shiftKey
+                false, // metaKey
+                identifier,
+                null // relatedTarget
+            );
+            event.touches = touchList;
+            event.targetTouches = touchList;
+            event.changedTouches = touchList;
+            event.scale = 0;
+            event.rotation = 0;
+            return event;
+        };
+    }
+    // create touch event for android stock browser spec
+    else if (env.isAndroid && !env.isChrome) {
+        return function(type, target, bubbles, cancelable, pageX, pageY, identifier) {
+            var event = document.createEvent('MouseEvent');
+            var touchList = _getTouchList(target, identifier, pageX, pageY);
+
+            event.initTouchEvent(
+                touchList,
+                touchList,
+                touchList,
+                type,
+                _defaultView,
+                0, // screenX
+                0, // screenY
+                pageX,
+                pageY,
+                false, // ctrlKey
+                false, // altKey
+                false, // shiftKey
+                false  // metaKey
+            );
+            return event;
+        };
+    }
+    // w3c spec touch event
+    else {
+        return function(type, target, bubbles, cancelable, pageX, pageY, identifier) {
+            var event = document.createEvent('MouseEvent');
+            var touchList = _getTouchList(target, identifier, pageX, pageY);
+
+            event.initTouchEvent(
+                type,
+                bubbles,
+                cancelable,
+                _defaultView,
+                1, // detail
+                0, // screenX
+                0, // screenY
+                pageX,
+                pageY,
+                false, // ctrlKey
+                false, // altKey
+                false, // shiftKey
+                false, // metaKey
+                touchList,
+                touchList,
+                touchList,
+                0, // scale
+                0  // rotation
+            );
+            return event;
+        };
+    }
+
+    function _getTouchList(target, identifier, pageX, pageY) {
+        var touch = document.createTouch(
+                _defaultView,
+                target,
+                identifier,
+                pageX,
+                pageY,
+                0, // screenX
+                0, // screenY
+                0, // clientX
+                0, // clientY
+                0, // radiusX
+                0, // radiusY
+                0, // rotationAngle
+                1  // force
+        );
+
+        return document.createTouchList(touch);
+    }
+})(Paw.env);
 
 Paw.prototype = {
     constructor: Paw,
@@ -219,6 +327,42 @@ function _dispose(cond) {
 //// private methods
 function __getTouchInfoList(ev) {
     return IS_SUPPORT_TOUCH_EVENT && ev.changedTouches || [ev];
+}
+
+function __getEnvData(ua) {
+    ua = ua.toLowerString();
+
+    var appleWebkitRE = /applewebkit/;
+    var res = {
+        isAndroid : /android/.test(ua),
+        isIOS     : /ip(hone|od|ad)/.test(ua),
+        isChrome  : /(chrome|crios)/.test(ua),
+        versionString: null,
+        version: null
+    };
+    var version, versionString;
+
+    res.isAndroidBrowser = !res.isChrome && res.isAndroid && appleWebkitRE.test(ua);
+    res.isMobileSafari = !res.isChrome && res.isIOS && appleWebkitRE.test(ua);
+
+    versionString =
+        (res.isAndroidBrowser || res.android && res.chrome) ? ua.match(/android\s(\S.*?)\;/) :
+        (res.isMobileSafari || res.isIOS && res.chrome) ? ua.match(/os\s(\S.*?)\s/) :
+        null;
+    if (versionString) {
+        if (res.isIOS) {
+            versionString = versionString[1].replace('_', '.');
+        } else {
+            versionString = versionString[1];
+        }
+        version = versionString.split('.');
+        for (var i = 0, iz = version.length; i < iz; i++) {
+            version[i] = version[i]|0;
+        }
+        res.versionString = versionString;
+        res.version = version;
+    }
+    return res;
 }
 
 // exports
